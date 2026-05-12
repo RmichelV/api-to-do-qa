321a0471e39e9be8dc48557af18b3d5b31256a5f
Richard Villalba <richard.villalba.mendieta@gmail.com>
2026-05-12 09:21:02 -0400

    cambiando el puerto

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ import { activeBrowsersAnchor } from './services/anchorReadingService.js'
 import { activeBrowsersFull } from './services/fullAnalysisService.js'
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json({ limit: '5mb' }));
 
